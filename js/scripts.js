5f956957921d228f297f948c3fc09415e64ca661
D-S1808 <domenicoscazziota@icloud.com>
2026-02-25 18:33:10 +0100

    eight commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
 function test() {
     console.log("test")
 };
+
+const images = [
+    "/pictures/AI_man.png",
+    "/pictures/AI_man.png"
+
+];
+
+const gallery =document.getElementById("gallery");
+
+images.forEach(src=> {
+    const img = document.createElement("img");
+    img.src = src;
+    img.classList.add("w-full", "h-auto", "object-cover", "rounded-lg", "shadow-lg", "transition-transform", "duration-500", "hover:scale-105");
+    gallery.appendChild(img);
+});
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+gallery.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        lightboxImg.src = e.target.src;
+        lightbox.classList.remove("hidden");
+    }
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.classList.add("hidden");
+});
