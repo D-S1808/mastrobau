58559709f24ff807b72ea7f0cf86d421f9c6d0d9
D-S1808 <domenicoscazziota@icloud.com>
2026-03-01 23:47:25 +0100

    modified myself.html and tuning  jobs.html for gallery

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,18 @@ function test() {
 };
 
 const images = [
+    "/pictures/jobs.png",
     "/pictures/AI_man.png",
     "/pictures/AI_man.png"
 
 ];
 
-const gallery =document.getElementById("gallery");
+const gallery = document.getElementById("gallery");
 
 images.forEach(src=> {
     const img = document.createElement("img");
     img.src = src;
-    img.classList.add("w-full", "h-auto", "object-cover", "rounded-lg", "shadow-lg", "transition-transform", "duration-500", "hover:scale-105");
+    img.classList.add("w-full", "h-64", "object-cover", "rounded-lg", "shadow-lg", "transition-transform", "duration-500", "hover:scale-105");
     gallery.appendChild(img);
 });
 
